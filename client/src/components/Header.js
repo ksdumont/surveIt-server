bbbd55b19ff76bff003c26f12c6a3fc9cf79f4fb
ksdumont <ksdumont@gmail.com>
2020-04-12 13:49:27 -0700

    added redux and router

---
+++ client/src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            SurveIt
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {this.renderContent()}
+            {/* <li>
+                  <a href="badges.html">Components</a>
+                </li>
+                <li>
+                  <a href="collapsible.html">JavaScript</a>
+                </li> */}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>SurveIt!</h1>
+      <h3>Collect feedback from your users</h3>
+    </div>
+  );
+};
+export default Landing;
