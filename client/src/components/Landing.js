8eec7e87080321eda4e554ee7211a0ab6407cea9
ksdumont <ksdumont@gmail.com>
2020-04-17 16:35:30 -0700

    added image

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,6 +5,11 @@ const Landing = () => {
     <div style={{ textAlign: "center" }}>
       <h1>SurveIt!</h1>
       <h3>Collect feedback from your users</h3>
+      <img
+        src="./surveyImage.jpg"
+        alt="survey"
+        style={{ width: "70%", margin: "0 auto"}}
+      />
     </div>
   );
 };
